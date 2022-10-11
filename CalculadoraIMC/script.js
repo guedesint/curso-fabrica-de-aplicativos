777c42b2cb193a80a63fb3d81f21bfbf53438ebc
guedesint <guedesint@gmail.com>
2022-10-10 21:43:22 -0300

    Fabrica de aplicativo

---
+++ CalculadoraIMC/script.js
@@ -0,0 +1,29 @@
+var peso;
+var altura;
+var imc;
+var resultado;
+
+function calcular(event) {
+    event.preventDefault();
+
+    peso = document.getElementById('peso').value;
+    altura = document.getElementById('altura').value;
+
+    imc = peso / (altura * altura);
+    resultado = document.getElementById('resultado');
+
+    if (imc < 17) {
+        resultado.innerHTML = '<br/> Seu resultado é ' + imc.toFixed(2) + '<br/> Cuidado voce esta muito abaixo do peso';
+    } else if (imc > 17 && imc <= 18.49) {
+        resultado.innerHTML = '<br/> Seu resultado é ' + imc.toFixed(2) + '<br/> Voce esta abaixo do peso';
+    } else if (imc > 18.5 && imc <= 24.99) {
+        resultado.innerHTML = '<br/> Seu resultado é ' + imc.toFixed(2) + '<br/> Voce esta no peso ideal';
+    } else if (imc > 25 && imc <= 29.99) {
+        resultado.innerHTML = '<br/> Seu resultado é ' + imc.toFixed(2) + '<br/> Voce esta acima do peso';
+    } else if (imc >= 30) {
+        resultado.innerHTML = '<br/> Seu resultado é ' + imc.toFixed(2) + '<br/> Cuidado voce esta obeso';
+    }
+
+    document.getElementById('peso').value = '';
+    document.getElementById('altura').value = '';
+}
